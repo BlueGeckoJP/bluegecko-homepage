0d88d10ec0865fbd832b8430cfe69a15ffda0c23
bluegecko <kei09doya@gmail.com>
2025-03-02 15:45:33 +0900

    add animated background shapes and update styles for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const shapes = document.querySelectorAll(".background-shapes li");
+    shapes.forEach((shape) => {
+      const s = shape as HTMLElement;
+      s.style.left = `${Math.random() * 100}vw`;
+      s.style.animationDuration = `${Math.random() * 2 + 1}s`;
+      s.style.animationDelay = `${Math.random() * 2}s`;
+
+      const size = Math.random() * 5 + 1;
+      s.style.width = `${size}rem`;
+      s.style.height = `${size}rem`;
+    });
+  }, []);
+
   return (
     <>
+      <ul className="background-shapes">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
       <div className="app">
         <div className="centre-container">
           <header className="app-header">
