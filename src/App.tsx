7f1977da7ce0dc2706d4402628b0e00db79cb94e
bluegecko <kei09doya@gmail.com>
2025-03-02 17:20:26 +0900

    update HTML title and integrate Vercel Analytics and Speed Insights components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { useEffect } from "react";
 import "./App.css";
 import profileImage from "./assets/profile-icon.png";
@@ -126,6 +128,8 @@ function App() {
           </span>
         </div>
       </div>
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -8,7 +6,5 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <Analytics />
-    <SpeedInsights />
   </StrictMode>
 );
