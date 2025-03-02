3a1813a4b9216c95d364a3561ee138abe6802341
bluegecko <kei09doya@gmail.com>
2025-03-02 17:39:48 +0900

    integrate Vercel Analytics and Speed Insights directly in App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -7,8 +5,6 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Analytics />
-    <SpeedInsights />
     <App />
   </StrictMode>
 );
