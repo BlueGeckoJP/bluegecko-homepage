0b79221b07991c2902fea32a4dd14e5a73b88dbb
bluegecko <kei09doya@gmail.com>
2025-03-02 15:08:18 +0900

    add FontAwesome icons and update App component with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <div className="centre-container">
+          <header className="app-header">
+            <span>BlueGecko</span>
+            <div className="nav-bar">
+              <a>Home</a>
+              <a>Works</a>
+              <div className="nav-with-icon">
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  color="#dde6ed"
+                ></FontAwesomeIcon>
+                <a>GitHub</a>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
