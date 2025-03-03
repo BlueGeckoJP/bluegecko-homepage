ae2bb209cf2017d77942d4dd44ede8f8fe470886
bluegecko <kei09doya@gmail.com>
2025-03-03 18:48:37 +0900

    add AnimatedOutlet component for view transitions and update routing for enhanced navigation experience

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { inject } from "@vercel/analytics";
 import { injectSpeedInsights } from "@vercel/speed-insights";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import AnimatedOutlet from "./AnimatedOutlet";
 import App from "./App";
 import Home from "./routes/Home";
 import Works from "./routes/Works";
@@ -13,8 +14,22 @@ createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="works" element={<Works />} />
+        <Route
+          index
+          element={
+            <AnimatedOutlet>
+              <Home />
+            </AnimatedOutlet>
+          }
+        />
+        <Route
+          path="works"
+          element={
+            <AnimatedOutlet>
+              <Works />
+            </AnimatedOutlet>
+          }
+        />
       </Route>
     </Routes>
   </BrowserRouter>
