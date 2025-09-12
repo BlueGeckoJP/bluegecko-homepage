ad27aa22b44071e03fd8c8522dfa1c6f5740750b
BlueGeckoJP <kei09doya@gmail.com>
2025-09-12 22:46:53 +0900

    feat: integrate HeroUI and update layout with providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { Button } from "@heroui/react";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <h1 className="text-4xl font-bold">It's a new homepage</h1>
+      <Button className="primary">Click me</Button>
     </div>
   );
 }
