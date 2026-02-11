e52aa0068abe56ecf4c87d96b5213a4f64e9b2a8
BlueGeckoJP <kei09doya@gmail.com>
2026-02-12 06:12:49 +0900

    feat: update about and projects sections with new information and links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 <main
 	class="m-0 h-screen w-screen overflow-x-hidden overflow-y-auto bg-base leading-loose text-text-main"
 >
-	<div class="mx-auto h-screen w-1/2 py-24">
+	<div class="mx-auto h-fit w-1/2 py-24">
 		<section id="about">
 			<div class="flex items-center">
 				<img
@@ -32,6 +32,10 @@
 				/>
 				I am studying English to study abroad, and I am also studying French a little as a hobby.<br
 				/>
+				<br />
+				I am currently working at a company founded by my colleagues.<br />
+				I am responsible for frontend/backend development, mobile app development, and website/web-service
+				deployment.<br />
 			</span>
 			<span class="mt-8 mb-2 block pb-1 text-xl font-bold">What I Like</span>
 			<ul>
@@ -43,7 +47,57 @@
 				<li>- Vim/Neovim: I'm using the Vim Extension in VSCode.</li>
 			</ul>
 		</section>
-		<section id="projects"></section>
+		<section id="projects">
+			<span class="mt-8 mb-2 block border-b border-text-sub pb-1 text-xl font-bold"
+				>My Projects</span
+			>
+			<div class="mt-4 mb-4 flex gap-1">
+				-
+				<div class="flex flex-col">
+					<span>
+						<a href="https://kopi-chat.com" class="border-accent text-accent hover:border-b"
+							>Kopi Chat (kopi-chat.com)</a
+						>:
+					</span>
+					<br />Bulletin Board Community for Japanese students studying in Malaysia.<br />I am
+					responsible for deploying the website.<br />
+					Please visit and use it if you are interested! :D
+				</div>
+			</div>
+			<ul>
+				<li>
+					- <a
+						href="https://github.com/BlueGeckoJP/rustic-viewer"
+						class="border-accent text-accent hover:border-b">rustic-viewer</a
+					>: An experimental image viewer built with Tauri/React.
+				</li>
+				<li>
+					- <a
+						href="https://github.com/BlueGeckoJP/gridx2"
+						class="border-accent text-accent hover:border-b">gridx2</a
+					>: The successor project of parax2. faster and simpler.
+				</li>
+				<li>
+					- <a
+						href="https://github.com/BlueGeckoJP/FramelessViewer"
+						class="border-accent text-accent hover:border-b">FramelessViewer</a
+					>: Image viewer with tiling.
+				</li>
+				<li>
+					- <a
+						href="https://github.com/BlueGeckoJP/explotty"
+						class="border-accent text-accent hover:border-b">explotty</a
+					>: Combined terminal and file explorer application.
+				</li>
+				<li>
+					- <a
+						href="https://github.com/BlueGeckoJP/bluegecko-homepage"
+						class="border-accent text-accent hover:border-b">bluegecko-homepage</a
+					>: My homepage!
+				</li>
+			</ul>
+			<span class="mt-8 block">Other projects/repositories in my GitHub.</span>
+		</section>
 		<section id="contact">
 			<span class="mt-8 mb-2 block border-b border-text-sub pb-1 text-xl font-bold">Contact</span>
 			My GitHub:
